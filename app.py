1e4ec59a0287d806d3a9ccb9e96f37327e371468
Mike1ife <yitai77@gmail.com>
2023-12-28 17:32:26 +0800

    抽

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 
 import os
+import requests
+import random
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
@@ -33,12 +35,31 @@ def callback():
     return "OK"
 
 
+CLIENT_ID = "427bae956e65de4"
+ACCESS_TOKEN = "a93827221b1aaca669344e401c8375c6ccdd5ef4"
+endpoint = "https://api.imgur.com/3/account/me/images"
+headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+
+
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # echo
     msg = event.message.text
     message = TextSendMessage(text=msg)
     line_bot_api.reply_message(event.reply_token, message)
+
+    if message == "抽":
+        response = requests.get(endpoint, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            images = data["data"]
+            if images:
+                random_image = random.choice(images)
+                image_url = random_image["link"]
+                image_message = ImageSendMessage(
+                    original_content_url=image_url, preview_image_url=image_url
+                )
+                line_bot_api.reply_message(event.reply_token, image_message)
 
 
 if __name__ == "__main__":
