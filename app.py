b40d3529fee07de1136379a0ff29063cbcff486d
Mike1ife <yitai77@gmail.com>
2024-01-05 12:56:23 +0800

    uid

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def text_message(event):
     msg = event.message.text
 
     if msg == "uid":
-        text = event.source.userID
+        text = event.source.userId
         text_message = TextSendMessage(text=text)
         line_bot_api.reply_message(event.reply_token, text_message)
 
