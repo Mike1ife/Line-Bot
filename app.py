64584c646323b7ef0665406a44c707a93cfcedf1
Mike1ife <yitai77@gmail.com>
2024-01-06 22:33:51 +0800

    gs

--- app.py
+++ app.py
@@ -120,28 +120,77 @@ def cron_job():
 
 @app.route("/api/open_vote_form", methods=["GET"])
 def open_vote_form():
-    team1 = urllib.parse.quote("公鹿")
-    team2 = urllib.parse.quote("馬刺")
-    carousel_template = CarouselTemplate(
-        columns=[
-            CarouselColumn(
-                thumbnail_image_url=f"https://raw.githubusercontent.com/Mike1ife/Line-Bot/main/images/merge/{team1}_{team2}.png",
-                title="公鹿 25-10 - 馬刺 5-29",
-                text="預測贏球球隊",
-                actions=[
-                    PostbackAction(label="公鹿", data="公鹿"),
-                    PostbackAction(label="馬刺", data="馬刺"),
-                ],
-            ),
-        ]
-    )
+    user_id = GROUP_ID
 
-    template_message = TemplateSendMessage(
-        alt_text="Vote for NBA Teams", template=carousel_template
-    )
+    time = None
+    UTCnow = datetime.utcnow().replace(tzinfo=timezone.utc)
+    TWnow = UTCnow.astimezone(timezone(timedelta(hours=8)))
+    time = f"{TWnow.year}-{TWnow.month}-{TWnow.day}"
 
-    line_bot_api.push_message(GROUP_ID, template_message)
-    return "Vote form opened successfully!"
+    data = requests.get(f"https://www.foxsports.com/nba/scores?date={time}").text
+    soup = BeautifulSoup(data, "html.parser")
+    team_rows = soup.find_all(class_="score-team-row")
+
+    team1 = {"name": "x", "standing": "x"}
+    team2 = {"name": "x", "standing": "x"}
+
+    i = 1
+    score_text = "NBA Today:\n"
+    columns = []
+
+    for team_row in team_rows:
+        team_name_elements = team_row.find_all(class_="score-team-name team")
+        team = team_name_elements[0].get_text() if team_name_elements else None
+        team = team.split()
+        team_name = nba_team_translations[team[0]]
+        if team[0] == "TRAIL":
+            team_standing = team[2]
+        else:
+            team_standing = team[1]
+
+        if i == 1:
+            team1["name"] = team_name
+            team1["standing"] = team_standing
+            i += 1
+        else:
+            team2["name"] = team_name
+            team2["standing"] = team_standing
+
+            encoded_team1 = urllib.parse.quote(team1["name"])
+            encoded_team2 = urllib.parse.quote(team2["name"])
+            thumbnail_image_url = f"https://raw.githubusercontent.com/Mike1ife/Line-Bot/main/images/merge/{encoded_team1}_{encoded_team2}.png"
+            if not check_url_exists(thumbnail_image_url):
+                thumbnail_image_url = f"https://raw.githubusercontent.com/Mike1ife/Line-Bot/main/images/merge/{encoded_team2}_{encoded_team1}.png"
+                team1, team2 = team2, team1
+
+            columns.append(
+                CarouselColumn(
+                    thumbnail_image_url=thumbnail_image_url,
+                    title=f"{team1['name']} {team1['standing']} - {team2['name']} {team2['standing']}",
+                    text="預測贏球球隊",
+                    actions=[
+                        PostbackAction(label=team1["name"], data=team1["name"]),
+                        PostbackAction(label=team2["name"], data=team2["name"]),
+                    ],
+                ),
+            )
+
+            score_text += f"{team1['name']} {team1['standing']} - {team2['name']} {team2['standing']}\n"
+
+            i = 1
+
+    text_message = TextSendMessage(text=score_text[:-1])
+    line_bot_api.push_message(user_id, text_message)
+
+    for i in range(0, len(columns), 10):
+        chunk = columns[i : i + 10]
+        carousel_template = CarouselTemplate(columns=chunk)
+        template_message = TemplateSendMessage(
+            alt_text="每日NBA預測", template=carousel_template
+        )
+        line_bot_api.push_message(user_id, template_message)
+
+    return "Cron job executed successfully!"
 
 
 @line_handler.add(PostbackEvent)
---
+++ tools/_user_table.py
@@ -0,0 +1,69 @@
+from google.oauth2.service_account import Credentials
+import gspread
+import pandas as pd
+import numpy as np
+
+
+def init():
+    scope = ["https://www.googleapis.com/auth/spreadsheets"]
+    creds = Credentials.from_service_account_file("gs_credentials.json", scopes=scope)
+    gs = gspread.authorize(creds)
+
+    sheet = gs.open_by_url(
+        "https://docs.google.com/spreadsheets/d/1QajQuyDTjBiaoj1ucQOfbZu99ChWLIRffABoKFohw3A/edit?hl=zh-tw#gid=0"
+    )
+    worksheet = sheet.get_worksheet(0)
+    data = worksheet.get_all_values()
+
+    df = pd.DataFrame(data[1:], columns=data[0])
+    return df, worksheet
+
+
+def modify_column_name(df, index, new_name):
+    # new column
+    if (index + 2) == len(df.columns):
+        df.insert(loc=index + 2, column=new_name, value="")
+    else:
+        df.columns.values[index + 2] = new_name
+    return df
+
+
+def check_user_exist(df, name):
+    return True if name in df["Name"].values else False
+
+
+def add_new_user(df, name):
+    new_row = {"Name": name, "Points": "0"}
+    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    df.replace({np.nan: ""}, inplace=True)
+    return df
+
+
+def reset_match(df):
+    df.drop(df.iloc[:, 2:], inplace=True, axis=1)
+    return df
+
+
+def modify_value(df, name, column, value):
+    df.at[df.index[df["Name"] == name].tolist()[0], column] = value
+    return df
+
+
+def count_points(df, name):
+    user_points = 0
+    for index, row in df[df["Name"] == name].iterrows():
+        for keys, values in row.items():
+            if keys != "Name":
+                if keys == "Points":
+                    user_points = int(values)
+                elif values == keys:
+                    user_points += 10
+
+    df = modify_value(df, name, "Points", user_points)
+    return df
+
+
+def update_sheet(df, worksheet):
+    modified_data = [df.columns.tolist()] + df.values.tolist()
+    worksheet.clear()
+    worksheet.update("A1", modified_data)
