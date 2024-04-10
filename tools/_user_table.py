e1798bf5857eaf2e63e22dec6d963f42b6426f86
Mike1ife <yitai77@gmail.com>
2024-04-10 22:16:19 +0800

    No ceded points

--- tools/_user_table.py
+++ tools/_user_table.py
@@ -304,12 +304,15 @@ def get_nba_today():
     match_index = 0
     values = soup.find_all("span", class_="secondary-text status ffn-11 opac-5 uc")
     for value in values:
-        team_name, team_give = value.text.strip().split()
-        match = matches[match_index]["name"]
-        team_to_give = match.index(nba_team_translations[team_name])
-        points = [0, 0]
-        points[team_to_give] = int(round(20 + float(team_give)))
-        points[1 ^ team_to_give] = int(round(20 + -float(team_give)))
+        try:
+            team_name, team_give = value.text.strip().split()
+            match = matches[match_index]["name"]
+            team_to_give = match.index(nba_team_translations[team_name])
+            points = [0, 0]
+            points[team_to_give] = int(round(20 + float(team_give)))
+            points[1 ^ team_to_give] = int(round(20 + -float(team_give)))
+        except:
+            points = [20, 20]
 
         matches[match_index]["points"] = points
         match_index += 1
