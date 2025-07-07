0f3eafc7e7801849f5cf3f7d093e2c33b7735122
Mike Tai <yitai77@gmail.com>
2025-07-08 02:26:15 +0800

    Fix(config.py): Wrong DB URL

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from linebot import LineBotApi, WebhookHandler
 
 LINE_BOT_API = LineBotApi(getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 HANDLER = WebhookHandler(getenv("LINE_CHANNEL_SECRET"))
-DATABASE_URL = "postgres://neondb_owner:npg_HL8gqNJc5kuf@ep-muddy-mountain-a4gxds6c-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+DATABASE_URL = getenv("DATABASE_URL")
