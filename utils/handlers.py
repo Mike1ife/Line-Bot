e30955c80408726d87b66b036ad3eeca63fbb812
Mike Tai <yitai77@gmail.com>
2025-07-27 21:49:14 +0800

    Fix: handler params

--- utils/handlers.py
+++ utils/handlers.py
@@ -29,8 +29,8 @@ def handle_postback(event: PostbackEvent):
     # NBA球隊預測: winner, loser, winner_point, loser_point, gametime(2025-3-18-11:51)
     # NBA球員預測: player, target, over_point, under_point, predict, match_time (Anthony Edwards 得分26.5 4 6 大盤 2025-3-18-11:51)
     if postbackType == "NBA球隊預測":
-        response = get_nba_prediction_posback(userName, userUID * params)
+        response = get_nba_prediction_posback(userName, userUID, *params)
         LINE_BOT_API.reply_message(event.reply_token, TextSendMessage(text=response))
     if postbackType == "NBA球員預測":
-        response = get_player_stat_prediction_postback(userName, userUID * params)
+        response = get_player_stat_prediction_postback(userName, userUID, *params)
         LINE_BOT_API.reply_message(event.reply_token, TextSendMessage(text=response))
