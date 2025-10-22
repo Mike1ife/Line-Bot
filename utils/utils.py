a6560393bec3083ff80da61b5139559956bc1fc3
Mike1ife <yitai77@gmail.com>
2025-10-21 22:18:28 -0400

    Test

--- utils/utils.py
+++ utils/utils.py
@@ -287,6 +287,8 @@ def get_nba_prediction_posback(
     nowTW = nowUTC.astimezone(timezone(timedelta(hours=8)))
     nowTWStr = nowTW.strftime("%Y-%m-%d-%H:%M")
     gameTimeStr = f"{gameDate}-{gameTime}"
+    print(gameTimeStr)
+    print(nowTWStr)
     if _compare_timestring(timeStr1=nowTWStr, timeStr2=gameTimeStr):
         return f"{team1Name}-{team2Name} 的比賽已經開始了"
 
