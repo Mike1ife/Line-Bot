db38e8d7060fe02999427c42b4d734ea6113c173
Mike1ife <132564989+Mike1ife@users.noreply.github.com>
2025-10-22 09:43:52 -0400

    Add team1Name and team2Name to SQL_SELECT_MATCH_ID

--- utils/_user_table.py
+++ utils/_user_table.py
@@ -67,7 +67,7 @@ def insert_player_stat_bet(playerStatBetList: list):
         ) in playerStatBetList:
             cur.execute(
                 SQL_SELECT_MATCH_ID,
-                (gameDate, team1Name, team2Name, team1Name, team2Name),
+                (gameDate, team1Name, team2Name, team1Name, team2Name, team1Name, team2Name),
             )
             matchID = cur.fetchone()[0]
             cur.execute(
@@ -86,7 +86,7 @@ def insert_user_predict_match(
 ):
     conn = _get_connection()
     with conn.cursor() as cur:
-        cur.execute(SQL_SELECT_MATCH_ID, (gameDate, team1Name, team2Name))
+        cur.execute(SQL_SELECT_MATCH_ID, (gameDate, team1Name, team2Name, team1Name, team2Name))
         matchID = cur.fetchone()[0]
         cur.execute(
             SQL_INSERT_USER_PREDICT_MATCH,
